9e2a989d3a044c4bc1c9a5fd036e9f2e8d56970b
t0dysseus <mdxppbzwdt@privaterelay.appleid.com>
2026-07-09 23:04:47 +0200

    Add files via upload

--- presentation-data.js
+++ presentation-data.js
@@ -164,7 +164,7 @@ dannenmann1: {
 dannenmann2: {
     title: "Dannenmann",
     image: "images/original.jpeg",
-    models: ["models/original.glb", "models/cut.glb",]
+    models: ["models/original.glb", "models/cut.glb",],
     gif: "images/3d.gif"
 },
 dannenmann3: {
